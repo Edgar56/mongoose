5b463c0c95aa725b75fefb30bc8aa6a5858e48e2
VerGil <edgaras.chalilovas@gmail.com>
2018-04-01 23:12:20 +0300

    Section 7 42 lecture

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-var MongoClient = require('mongodb').MongoClient
+const mongoose = require('mongoose');
 
-MongoClient.connect('mongodb://localhost:27017/animals', function (err, db) {
-    if (err) throw err
+mongoose.connect('mongodb://localhost:27017/animals');
+mongoose.connection
+    .once('open', ()=>console.log('Connected'))
+    .on('error', (err)=>{
+        console.log('could not connect', err);
+    });
 
-    console.log('Connected');
 
-    // db.collection('mammals').find().toArray(function (err, result) {
-    //     if (err) throw err
-    //
-    //     console.log(result)
-    // })
-});
+
+
+
+// var MongoClient = require('mongodb').MongoClient
+//
+// MongoClient.connect('mongodb://localhost:27017/animals', function (err, db) {
+//     if (err) throw err
+//
+//     console.log('Connected');
+//
+//     // db.collection('mammals').find().toArray(function (err, result) {
+//     //     if (err) throw err
+//     //
+//     //     console.log(result)
+//     // })
+// });
