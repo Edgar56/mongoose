7feee6673f109bb02834de7aa5f2b7a37fc127f7
Edgar56 <edgaras.chalilovas@gmail.com>
2018-04-04 22:16:31 +0300

    Creating model with mongoose

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const mongoose = require('mongoose');
+const express = require('express');
 
-mongoose.connect('mongodb://localhost:27017/animals');
+const User = require ('./models/User');
+
+
+mongoose.connect('mongodb://localhost:27017/mongoose');
 mongoose.connection
     .once('open', ()=>console.log('Connected'))
     .on('error', (err)=>{
         console.log('could not connect', err);
     });
 
+const newUser = new User ({
+
+    firstName:'E',
+    lastName: 'Chalilov'
+
+});
+
+newUser.save(function (err,dataSaved)
+{
+
+    if(err) return console.log(err);
+
+    console.log(dataSaved);
 
 
-
-
-// var MongoClient = require('mongodb').MongoClient
-//
-// MongoClient.connect('mongodb://localhost:27017/animals', function (err, db) {
-//     if (err) throw err
-//
-//     console.log('Connected');
-//
-//     // db.collection('mammals').find().toArray(function (err, result) {
-//     //     if (err) throw err
-//     //
-//     //     console.log(result)
-//     // })
-// });
+});
---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+//const Schema = mongoose.Schema;
+
+const User = mongoose.model('users', {
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 4,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 4,
+        trim: true
+    },
+    isActive: {
+        type: Number,
+        default: 0
+    }
+});
+
+module.exports= User;
